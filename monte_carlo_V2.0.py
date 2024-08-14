4496e5f95fda83e2a81878ad855eb34166a6b77f
Annedrew <1196244890@qq.com>
2024-08-14 12:25:34 +0200

    Automate activity chosen and format the code a bit.

--- monte_carlo_V2.0.py
+++ monte_carlo_V2.0.py
@@ -4,30 +4,38 @@ import bw2data as bd #this is needed for brightway 2.5 package
 from scipy import sparse #This is necessary to create the sparse matrix, which is a lighter matrix in which zero values have been removed.
 import pandas as pd
 import numpy as np
+from random import sample
+import random
 import os
 from statistic_analysis import StatisticAnalysis
 
 
 class SimulationScript:
-    def __init__(self):
-        pass
-
-
-    # Build matrix adapt to bw
-    def build_bw_matrix(self, A_file_path, S_file_path):
+    # Get all activities
+    def get_activities(self, A_file_path):
         A_raw = pd.read_table(A_file_path)
-
         countries = A_raw['region'].drop_duplicates().iloc[2:].tolist()
         sectors = list(A_raw.iloc[2:,1].drop_duplicates())
         activities = [ x + '-' + y for x in countries for y in sectors]
-        units = (['MEUR' for i in range(0,len(activities))])
-        extensions = ["349b29d1-3e58-4c66-98b9-9d1a076efd2e", "0795345f-c7ae-410c-ad25-1845784c75f5", "20185046-64bb-4c09-a8e7-e8a9e144ca98", "0795345f-c7ae-410c-ad25-1845784c75f5",
-        "0795345f-c7ae-410c-ad25-1845784c75f5", "0795345f-c7ae-410c-ad25-1845784c75f5", "0795345f-c7ae-410c-ad25-1845784c75f5", "0795345f-c7ae-410c-ad25-1845784c75f5",
-        "0795345f-c7ae-410c-ad25-1845784c75f5", "0795345f-c7ae-410c-ad25-1845784c75f5", "0795345f-c7ae-410c-ad25-1845784c75f5", "349b29d1-3e58-4c66-98b9-9d1a076efd2e",
-        "349b29d1-3e58-4c66-98b9-9d1a076efd2e", "35d1dff5-b535-4628-9826-4a8fce08a1f2", "e5ba3517-a93f-422e-9ce2-f1007fcf6c06", "0795345f-c7ae-410c-ad25-1845784c75f5",
-        "349b29d1-3e58-4c66-98b9-9d1a076efd2e", "20185046-64bb-4c09-a8e7-e8a9e144ca98", "0795345f-c7ae-410c-ad25-1845784c75f5", "349b29d1-3e58-4c66-98b9-9d1a076efd2e", "349b29d1-3e58-4c66-98b9-9d1a076efd2e"
-        ]
 
+        return activities
+
+
+    # Choose activities for experiments
+    def choose_activities(self, activities, amount):
+        chosen_activities = []
+        indices = sample(range(len(activities) - 1), amount)
+
+        chosen_activities = [(activities[i], i) for i in indices]
+
+        return chosen_activities
+    
+
+    # Build matrix adapt to bw
+    def build_bw_matrix(self, A_file_path, S_file_path):
+        activities = self.get_activities(A_file_path) # Get all activities
+
+        A_raw = pd.read_table(A_file_path)
         A_IO = A_raw.iloc[2:,2:].astype('float').values
         I = np.identity(len(A_IO))
         A_ = I - A_IO
@@ -39,6 +47,7 @@ class SimulationScript:
         a_indices = np.array([tuple(coord) for coord in np.transpose(Asparse.nonzero())], dtype=bwp.INDICES_DTYPE) # indices of each exchange
         a_flip = np.array([False if i[0] == i[1] else True for i in a_indices ]) # Numerical sign of the inputs needs to be flipped negative
 
+        # import environemntal extensions
         S_raw = pd.read_table(S_file_path, header=[0,1], index_col=[0])
 
         GHG_rows = ["CO2 - combustion - air",
@@ -72,7 +81,7 @@ class SimulationScript:
         b_indices_remap = [[i[0] + len(activities),i[1]] for i in np.transpose(Bsparse.nonzero())] # need to make sure biosphere indices are different from technosphere
         b_indices = np.array([tuple(coord) for coord in b_indices_remap], dtype=bwp.INDICES_DTYPE)
 
-        # matrix of charachterisation factors (method E.F. 3.1 - Climate change)
+        # matrix of charachterisation factors (method E.F. 3.1 - Climate change): CFs = [bw.ef.co2, bw.sox, ..., ]
         CFs = [1., 29.8, 273., 29.8, 29.8, 29.8, 29.8, 29.8, 29.8, 29.8, 29.8, 1., 1., 25200., 14600., 29.8, 1., 273., 29.8, 1., 1.]
 
         C = np.matrix(np.zeros((len(CFs), len(CFs))))
@@ -84,12 +93,12 @@ class SimulationScript:
         c_indices_remap = [[i[1] + len(activities),i[1]+ len(activities)] for i in np.transpose(Csparse.nonzero())] # same indices as in B
         c_indices = np.array([tuple(coord) for coord in c_indices_remap], dtype=bwp.INDICES_DTYPE) 
 
-        return activities, A, A_IO, B, a_data, b_data, c_data, a_indices, b_indices, c_indices, a_flip
+        return A, A_, A_IO, B, C, a_data, b_data, c_data, a_indices, b_indices, c_indices, a_flip
     
-    
+
     # Perform baseline simulation
-    def baseline_lca(self, activities, a_data, b_data, c_data, a_indices, b_indices, c_indices, a_flip, A, A_, B, C):
-        # Creating the datapackage
+    def perform_baseline(self, myact, index, a_data, b_data, c_data, a_indices, b_indices, c_indices, a_flip, A, A_, B, C):
+        # Creating a datapackage
         dp_static = bwp.create_datapackage()
         dp_static.add_persistent_vector(
             matrix='technosphere_matrix',
@@ -108,22 +117,21 @@ class SimulationScript:
             data_array=c_data,
         )
 
-        myact = activities[1] # to find the desired code for the functional unit
-        print(myact)
-
         lca = bc.LCA(
             demand={activities.index(myact): 1}, # using index because the argument of FU is an INTEGER
             data_objs=[dp_static],
         )
         lca.lci()
         lca.lcia()
-        lca.score
 
-        f = np.zeros(len(A)) # check with normal matrix operation
-        f[1] = 1 # functional unit
+        # check with normal matrix operation
+        f = np.zeros(len(A))
+        f[index] = 1 # functional unit
 
         print('ioscore',np.sum(C.dot(B.dot((np.linalg.inv(A_)).dot(f))))) # matrix operation
-        print('lca score',lca.score)  # similar values, when rounded. All is good
+        print('LCA score: ',lca.score)  # similar values, when rounded. All is good
+
+        return lca
 
 
     # -------------------------------------- STOCHASTIC LCA ----------------------------------------------
@@ -220,10 +228,7 @@ class SimulationScript:
     
 
     # Perform Monte Carlo simulation
-    def perform_simu(self, dp_stochastic, activities):
-        myact = activities[1] # to find the desired code for the functional unit
-        print(f"myact: {myact}")
-
+    def perform_simu(self, myact, dp_stochastic, activities):
         lca = bc.LCA(
             demand={activities.index(myact): 1},
             data_objs=[dp_stochastic],
@@ -231,22 +236,23 @@ class SimulationScript:
         )
         lca.lci()
         lca.lcia()
+
+        print('LCA score: ',lca.score)
         
         return lca
-
+    
 
     # Save results to csv
-    def save_result(self, directory, lca, k):
+    def save_result(self, directory, lca, k, myact):
         os.makedirs(directory, exist_ok=True)  # Create the directory if it does not exist
-        filename = os.path.join(directory, f"CASE_{k}_MC_simulations.csv") # Define filename for saving results
+        filename = os.path.join(directory, f"CASE_{k}_MC_simulations_{myact}.csv") # Define filename for saving results
         
         # Define simulation parameters
         batch_size = 50
         num_batches = 10
 
-        file.write("kg CO2eq\n") # Write the header
-
         with open(filename, "w") as file:
+            file.write("kg CO2eq\n") # Write the header
             for p in range(num_batches):
                 # Run simulations for the current batch
                 batch_results = [lca.score for _ in zip(range(batch_size), lca)]
@@ -264,78 +270,87 @@ class SimulationScript:
 
 
 if __name__ == "__main__":
-    # Monte Carlo simulation Description:
-        # CASE 1: uniform distribution with 10% uncertainty
-        # CASE 2: uniform distribution with 20% uncertainty
-        # CASE 3: uniform distribution with 30% uncertainty
-        # CASE 4: log-normal distribution with 1.01 uncertainty
-        # CASE 5: log-normal distribution with 1.1 uncertainty 
-        # CASE 6: log-normal distribution with 2 uncertainty 
-
-    u_uniform = [0.1, 0.2, 0.3, 0.5]
-    u_log = [1.01, 1.1, 2]
-    dist_type = ["uniform", "log-normal", "baseline"]
+    # --------------------- Configuration --------------------- 
+    dist_type = ["baseline", "uniform", "log-normal"] # Define the types of distribution
+    u_uniform = [0.1, 0.2, 0.3, 0.5] # Define the uncertainty for uniform distribution
+    u_log = [1.01, 1.1, 2] # Define the uncertainty for log distribution
+    amount = 4 # This is the amount of activities for 1 CASE
 
     # File paths
-    dir_input = "/Users/bp45th/Documents/github/monte-carlo-analysis-exiobase-Brightway-/exiobase_2022_small"
-    dir_output = "/Users/bp45th/Documents/github/monte-carlo-analysis-exiobase-Brightway-/output"
-    A_file_path = "/Users/bp45th/Documents/github/monte-carlo-analysis-exiobase-Brightway-/exiobase_2022_small/A.txt"
-    S_file_path = "/Users/bp45th/Documents/github/monte-carlo-analysis-exiobase-Brightway-/exiobase_2022_small/satellite/S.txt"
+    dir_input = f"{os.getcwd()}/exiobase_2022_small"
+    dir_output = f"{os.getcwd()}/output"
+    A_file_path = f"{os.getcwd()}/exiobase_2022_small/A.txt"
+    S_file_path = f"{os.getcwd()}/exiobase_2022_small/satellite/S.txt"
     
     # Make sure execute in the right directory
     if os.getcwd() != dir_input:
         os.chdir(dir_input)
-    
     print("Directed to the right directory.")
-
-    simu = SimulationScript()
-
-    # Adapt matrices for bw
-    activities, A, A_IO, B, a_data, b_data, c_data, a_indices, b_indices, c_indices, a_flip = simu.build_bw_matrix(A_file_path, S_file_path)
-    print("Matrices is formatted.")
 
 
     # --------------------- Simulation --------------------- 
+    simu = SimulationScript()
+
+    # Choose activities
+    activities = simu.get_activities(A_file_path)
+    chosen_activities = simu.choose_activities(activities, amount)
+    print("The following activities are chosen:")
+    for myact, _ in chosen_activities:
+        print(myact, end="")
+    
+    # Adapt matrices for bw
+    A, A_, A_IO, B, C, a_data, b_data, c_data, a_indices, b_indices, c_indices, a_flip = simu.build_bw_matrix(A_file_path, S_file_path)
+    print("Matrices are formatted.")
+
+    # Run the simulation
     k = 0
     for t in dist_type:
-        # This is the uniform case
-        if t == "uniform":
-            for u in u_uniform:
+        # This is the baseline case
+        if t == "baseline":
+            for myact, index in chosen_activities:
                 k += 1
-                print(f"Starting CASE {k}.")
+                print(f"----------- Starting CASE {k}, activity: {myact} -----------")
+                lca = simu.perform_baseline(myact, index, a_data, b_data, c_data, a_indices, b_indices, c_indices, a_flip, A, A_, B, C)
+                simu.save_result(dir_output, lca, k, myact)
 
-                # Add uncertainty
-                dp_stochastic = simu.add_uncertainty(t, u, a_data, b_data, c_data, a_indices, b_indices, c_indices, a_flip)
-                print(f"Uncertainty added to CASE {k}")
-
-                # Perform lca
-                lca = simu.perform_simu(dp_stochastic, activities)
                 print(f"CASE {k} simulation is done.")
 
-                simu.save_result(dir_output, lca, k)
+        # This is the uniform case
+        elif t == "uniform":
+            for u in u_uniform:
+                for myact, index in chosen_activities:
+                    k += 1
+                    print(f"----------- Starting CASE {k}, activity: {myact} -----------")
 
+                    # Add uncertainty
+                    dp_stochastic = simu.add_uncertainty(t, u, a_data, b_data, c_data, a_indices, b_indices, c_indices, a_flip)
+                    print(f"Uncertainty added to CASE {k}")
+
+                    # Perform lca
+                    lca = simu.perform_simu(myact, dp_stochastic, activities)
+                    simu.save_result(dir_output, lca, k, myact)
+
+                    print(f"CASE {k} simulation is done.")
         # This is the log-normal case
         elif t == "log-normal":
             for u in u_log:
-                k += 1
-                print(f"Starting CASE {k}.")
+                for myact, index in chosen_activities:
+                    k += 1
+                    print(f"----------- Starting CASE {k}, activity: {myact} -----------")
+                    # Add uncertainty
+                    dp_stochastic = simu.add_uncertainty(t, u, a_data, b_data, c_data, a_indices, b_indices, c_indices, a_flip)
+                    print(f"Uncertainty added. ({t} distribution with uncertainty {u})")
 
-                # Add uncertainty
-                dp_stochastic = simu.add_uncertainty(t, u, a_data, b_data, c_data, a_indices, b_indices, c_indices, a_flip)
-                print(f"Uncertainty added. ({t} distribution with uncertainty {u})")
+                    lca = simu.perform_simu(myact, dp_stochastic, activities)
+                    simu.save_result(dir_output, lca, k, myact)
 
-                lca = simu.perform_simu(dp_stochastic, activities)
-                print(f"CASE {k} simulation is done.")
+                    print(f"CASE {k} simulation is done.")
 
-                simu.save_result(dir_output, lca, k)
-
-        elif t == "baseline":
-            pass
 
     print("All simulations completed.")
 
 
     # --------------------- Statistic Analysis --------------------- 
-    stat = StatisticAnalysis()
-    stat.simu_plot()
-    stat.matrix_plot(A, A_IO, B)
+    # stat = StatisticAnalysis()
+    # stat.simu_plot()
+    # stat.matrix_plot(A, A_IO, B)
