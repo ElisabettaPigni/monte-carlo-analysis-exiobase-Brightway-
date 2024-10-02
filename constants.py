747f74d9b39f76f26133279dad1aad08948affc0
Annedrew <1196244890@qq.com>
2024-10-02 15:22:40 +0200

    Change another way to get path, and add some constants.

--- constants.py
+++ constants.py
@@ -1,16 +1,18 @@
 import os
 
+# ---------- PARAMETERS FOR SIMULATION ---------- 
+
 # BIG DATASET FILE PATH
-BIG_DIR_INPUT = f"{os.getcwd()}/IOT_2022_pxp"
-BIG_DIR_OUTPUT = f"{os.getcwd()}/big_output"
-BIG_A_FILE = f"{os.getcwd()}/IOT_2022_pxp/A.txt"
-BIG_S_FILE = f"{os.getcwd()}/IOT_2022_pxp/satellite/S.txt"
+BIG_DIR_INPUT = os.path.join(os.getcwd(), "IOT_2022_pxp")
+BIG_DIR_OUTPUT = os.path.join(os.getcwd(), "big_output")
+BIG_A_FILE = os.path.join(os.getcwd(), "IOT_2022_pxp", "A.txt")
+BIG_S_FILE = os.path.join(os.getcwd(), "IOT_2022_pxp", "satellite", "S.txt")
 
 # SMALL DATASET FILE PATH
-SMALL_DIR_INPUT = f"{os.getcwd()}/exiobase_2022_small"
-SMALL_DIR_OUTPUT = f"{os.getcwd()}/small_output"
-SMALL_A_FILE = f"{os.getcwd()}/exiobase_2022_small/A.txt"
-SMALL_S_FILE = f"{os.getcwd()}/exiobase_2022_small/satellite/S.txt"
+SMALL_DIR_INPUT = os.path.join(os.getcwd(), "exiobase_2022_small")
+SMALL_DIR_OUTPUT = os.path.join(os.getcwd(), "small_output")
+SMALL_A_FILE = os.path.join(os.getcwd(), "exiobase_2022_small", "A.txt")
+SMALL_S_FILE = os.path.join(os.getcwd(), "exiobase_2022_small", "satellite", "S.txt")
 
 # CHOSEN ACTIVITIES
 SMALL_CHOSEN_ACT = [("RoW-Services", 68), ("EU28-Biodiesels", 11), ("EU28-Agriculture-Forestry-Fishing", 0), ("EU28-Basic iron and steel and of ferro-alloys and first products thereof", 13)]
@@ -22,3 +24,9 @@ U_UNIFORM = [0.1, 0.2, 0.3] # Define the uncertainty for uniform distribution
 U_LOG = [1.1, 1.2, 1.3] # Define the uncertainty for log distribution
 AMOUNT = 4 # This is the amount of activities for 1 CASE
 
+# ---------- PARAMETERS FOR PLOT DRAWING ---------- 
+
+FOLDER_PATH = os.path.join(os.getcwd(), "Exiobase_folders_for_Ning")
+DB_SIZE = ["small", "big"]
+PLT_COMP_TYPE = ["cases", "sectors"]
+PLT_SAVE_PATH = os.path.join(FOLDER_PATH, "plt_output")
