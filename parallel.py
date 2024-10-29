45d87c0fd53999b48674ae57c9f56bf72a630cb0
Annedrew <1196244890@qq.com>
2024-10-29 09:08:10 +0100

    Polish code.

--- parallel.py
+++ parallel.py
@@ -6,13 +6,16 @@ from constants import *
 from monte_carlo_utility import *
 
 
-# ((t, "0", myact, index, k), matrices)
 def process_case(parallel_param):
+    """
+    parallel_param type: tuple
+    parallel_param format: ((t, "0", myact, index, k), matrices)
+    """
+    print(f"Processing {t}_{u} in process: {os.getpid()}")
+
     (t, u, myact, index, k) = parallel_param[0]
     matrices = parallel_param[1]
     A, A_, A_IO, B, C, a_data, b_data, c_data, a_indices, b_indices, c_indices, a_flip = matrices
-
-    print(f"Processing {t}_{u} in process: {os.getpid()}")
 
     simu = SimulationScript()
 
@@ -45,21 +48,21 @@ if __name__ == "__main__":
         if t == "baseline":
             for myact, index in BIG_CHOSEN_ACT:
                 k += 1
-                parallel_params.append(((t, 0, myact, index, k), matrices))
+                parallel_params.append((t, 0, myact, index, k))
         elif t == "uniform":
             for u in U_UNIFORM:
                 for myact, index in BIG_CHOSEN_ACT:
                     k += 1
-                    parallel_params.append(((t, u, myact, index, k), matrices))
+                    parallel_params.append((t, u, myact, index, k))
         elif t == "log-normal":
             for u in U_LOG:
                 for myact, index in BIG_CHOSEN_ACT:
                     k += 1
-                    parallel_params.append(((t, u, myact, index, k), matrices))
+                    parallel_params.append((t, u, myact, index, k))
     
     max_workers = os.cpu_count() if os.cpu_count() else 4
     with ProcessPoolExecutor(max_workers=max_workers) as executor:
-        futures = {executor.submit(process_case, parallel_param): parallel_param for parallel_param in parallel_params}
+        futures = {executor.submit(process_case, (parallel_param, matrices)): (parallel_param, matrices) for parallel_param in parallel_params}
         for future in as_completed(futures):
             try:
                 future.result()
