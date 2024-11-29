d2541377780164f2bd77798e6d199aa6ac0021b7
Annedrew <1196244890@qq.com>
2024-11-29 10:16:30 +0100

    Find and add pedigree uncertainty and etc.

--- monte_carlo_utility_new.py
+++ monte_carlo_utility_new.py
@@ -16,14 +16,19 @@ from typing import List, Tuple, Any
 
 class SimulationScript:
     def __init__(self):
-        self.metadata = {
-            "technosphere": {}, # technosphere_column_index
-            "biosphere": {}, # biosphere_column_index
-        }
+        """
+        * self.metadata: save technosphere data, including column index and gsd
+        """
+        self.metadata = []
 
-    def save_metadata(self, names, matrix_type):
-        for name in names:
-            self.metadata[matrix_type][name] = 0
+    def file_preprocessing(self, file_name, delimiter: str, column_name: str, expacted_order: list):
+        """
+        because human cannot put things always in the same order.
+        """
+        df = pd.read_csv(file_name, delimiter=delimiter)
+        df_sorted = df.set_index(column_name).reindex(expacted_order).reset_index()
+
+        return df_sorted
 
     def get_index(self, activities: list, activity_name: str) -> int:
         """
@@ -31,14 +36,6 @@ class SimulationScript:
         """
         index = activities.index(activity_name)
         return index
-
-    def get_activities(self, region_column: pd.Series, sector_column: pd.Series) -> list:
-        """
-        Form activities by combing <country_name> and <sector_name>.
-        """
-        activities = (region_column + "-" + sector_column).to_list()
-
-        return activities
     
     def form_tech_matrix(self, raw_tech: pd.DataFrame):
         identity_matrix = np.identity(len(raw_tech))
@@ -108,8 +105,106 @@ class SimulationScript:
         cf_matrix = np.diagflat(cf_matrix)
             
         return cf_matrix
+
+    def get_country_sector(self, activity):
+        """
+        Separate the country and sector.
+        """
+        country, sector = activity.split("-", 1)
+
+        return country, sector
+
+    def map_pedigree_uncertainty(self, country_file, sector_file, region_sector_file):
+        """
+        Build dictionaries to mapping specific uncertainty.
+        """
+        country_dfs = pd.read_csv(country_file, delimiter=";")
+        sector_dfs = pd.read_csv(sector_file, delimiter=";")
+        region_sector_dfs = pd.read_csv(region_sector_file, delimiter=";")
+
+        country_region = country_dfs.set_index(country_dfs.columns[0])[country_dfs.columns[1]].to_dict()
+        sector_seccat = sector_dfs.set_index(sector_dfs.columns[0])[sector_dfs.columns[1]].to_dict()
+
+        return country_region, sector_seccat, region_sector_dfs
+
+    def find_pedigree_uncertainty(self, activity, country_region, sector_seccat, region_sector_dfs):
+        """
+        Search for uncertainty for specific activity or biosphere flow.
+        """
+        country, sector = self.get_country_sector(activity)
+        region_category =  country_region.get(country, None)
+        sector_category = sector_seccat.get(sector, None)
+
+        if region_category !=  None and sector_category != None:
+            gsd = float(region_sector_dfs[(region_sector_dfs.iloc[:, 0] == region_category) & (region_sector_dfs.iloc[:, 1] == sector_category)]["GSD"].iloc[0])
+        else:
+            # print("No GSD found.")
+            gsd = None
+
+        return gsd
     
-    def prepare_bw_matrix(self, tech_matrix, bio_matrix, cf_matrix):
+    def calc_specific_uncertainty(self, data, uncertainty):
+        loc = np.log(data)
+        scale = np.log(uncertainty)
+
+        return loc, scale
+    
+    # TODO: design for pedigree and specific uncertainty
+    def add_uncertainty(self, bw_data, bw_indices, bw_flip):
+        bw_uncertainties = []
+        if bw_flip is not None: # technosphere
+            k = 0
+            for data, indices, flip in zip(bw_data, bw_indices, bw_flip):
+                uncertainty = list(self.metadata[indices[1]].items())[0][1][1]
+                if uncertainty is not None:
+                    if indices[1] == 0:
+                        uncertainty = uncertainty[k]
+                        k += 1
+                        if uncertainty == 0:
+                            parameters_a = (0, data, np.NaN, np.NaN, np.NaN, np.NaN, False)
+                        else:
+                            loc = np.log(data)
+                            scale = np.log(uncertainty)
+                            if not flip:
+                                parameters_a = (0, data, np.NaN, np.NaN, np.NaN, np.NaN, False)
+                            else:
+                                parameters_a = (2, loc, scale, np.NaN, np.NaN, np.NaN, False)
+                    else:
+                        loc = np.log(data)
+                        scale = np.log(uncertainty)
+                        if not flip:
+                            parameters_a = (0, data, np.NaN, np.NaN, np.NaN, np.NaN, False)
+                        else:
+                            parameters_a = (2, loc, scale, np.NaN, np.NaN, np.NaN, False)
+                else:
+                    parameters_a = (0, data, np.NaN, np.NaN, np.NaN, np.NaN, False)
+                bw_uncertainties.append(parameters_a)
+        else:
+            k = 0
+            for data, indices in zip(bw_data, bw_indices):
+                uncertainty = list(self.metadata[indices[1]].items())[0][1][1]
+                if uncertainty is not None:
+                    if indices[1] == 0:
+                        uncertainty = uncertainty[k]
+                        k += 1
+                        if uncertainty == 0:
+                            parameters_a = (0, data, np.NaN, np.NaN, np.NaN, np.NaN, False)
+                        else:
+                            loc = np.log(data)
+                            scale = np.log(uncertainty)
+                            parameters_a = (2, loc, scale, np.NaN, np.NaN, np.NaN, False)
+                    else:
+                        loc = np.log(data)
+                        scale = np.log(uncertainty)
+                        parameters_a = (2, loc, scale, np.NaN, np.NaN, np.NaN, False)
+                else:
+                    parameters_a = (0, data, np.NaN, np.NaN, np.NaN, np.NaN, False)
+                bw_uncertainties.append(parameters_a)
+
+        return np.array(bw_uncertainties, dtype=bwp.UNCERTAINTY_DTYPE)
+
+    
+    def prepare_bw_matrix(self, tech_matrix, bio_matrix, cf_matrix, activities):
         """
         Transform matrices data to bw matrices data, ready for the datapackages.
         """
@@ -124,20 +219,16 @@ class SimulationScript:
         bio_sparse = sparse.coo_array(bio_matrix)
         bio_coors = np.column_stack(bio_sparse.nonzero())
         bio_data = bio_sparse.data
-        bio_indices = np.array([tuple([coord[0] + max_coor[0], coord[1]]) for coord in bio_coors], dtype=bwp.INDICES_DTYPE)
+        bio_indices = np.array([tuple([coord[0] + max_coor[0] + 1, coord[1]]) for coord in bio_coors], dtype=bwp.INDICES_DTYPE)
         
         cf_sparse = sparse.coo_array(cf_matrix)
         cf_coors = np.column_stack(cf_sparse.nonzero())
         cf_data =  cf_sparse.data
-        cf_indices = np.array([tuple([coord[0] + max_coor[0], coord[1] + max_coor[1]]) for coord in cf_coors], dtype=bwp.INDICES_DTYPE)
-        
-        tech_poss = list(set(coord[1] for coord in tech_indices))
-        for act, tech_pos in zip(self.metadata["technosphere"], tech_poss):
-            self.metadata["technosphere"][act] = tech_pos
+        cf_indices = np.array([tuple([coord[0] + max_coor[0] + 1, coord[1] + max_coor[1] + 1]) for coord in cf_coors], dtype=bwp.INDICES_DTYPE)
 
-        bio_poss = list(set(coord[1] for coord in bio_indices))
-        for emission, bio_pos in zip(self.metadata["biosphere"], bio_poss):
-            self.metadata["biosphere"][emission] = bio_pos
+        tech_poss = list(set(coord[1] for coord in tech_indices))
+        for act, tech_pos in zip(activities, tech_poss):
+            self.metadata.append({tech_pos: act})
 
         return [
             (tech_data, tech_indices, tech_flip),
@@ -150,7 +241,7 @@ class SimulationScript:
         tech_data, tech_indices, tech_flip = datapackage_data[0]
         bio_data, bio_indices = datapackage_data[1]
         cf_data, cf_indices = datapackage_data[2]
-        # tech_uncertainty, bio_uncertainty, cf_uncerainty = uncertainty[0], uncertainty[1], uncertainty[2]
+        tech_uncertainty, bio_uncertainty, cf_uncerainty = uncertainty[0], uncertainty[1], uncertainty[2]
 
         dp = bwp.create_datapackage()
         dp.add_persistent_vector(
@@ -158,19 +249,19 @@ class SimulationScript:
             indices_array=tech_indices,
             data_array=tech_data,
             flip_array=tech_flip,
-            # distributions_array=tech_uncertainty,
+            distributions_array=tech_uncertainty,
         )
         dp.add_persistent_vector(
             matrix='biosphere_matrix',
             indices_array=bio_indices,
             data_array=bio_data,
-            # distributions_array=bio_uncertainty,
+            distributions_array=bio_uncertainty,
         )
         dp.add_persistent_vector(
             matrix='characterization_matrix',
             indices_array=cf_indices,
             data_array=cf_data,
-            # distributions_array=cf_uncerainty,
+            distributions_array=cf_uncerainty,
         )
 
         return dp
